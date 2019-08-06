64324c36e5fdd9464ac454c19611dd1a83b88d93
Klaudia Zarychta <52625587+klaudia-zarychta@users.noreply.github.com>
2019-08-07 00:43:07 +0200

    Add files via upload

---
+++ Recipe.java
@@ -0,0 +1,24 @@
+package com.infoshareacademy;
+
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Recipe {
+    private int id;
+    private String name;
+    private String instruction;
+    private String glass;
+    private String measure;
+    private String alcoholicOrNot;
+    Date modificationDate;
+
+    //Recipe Map   --> mapa przepisów ?
+    //List<String> category = new ArrayList<String>();
+   // List<String> ingredients = new ArrayList<String>();
+
+
+
+}
+
---
+++ RecipeSearcher.java
@@ -0,0 +1,19 @@
+package com.infoshareacademy;
+
+public class RecipeSearcher(){
+
+      /*  List<Recipe> recipe;
+
+
+        public List<Recipe> findRecipeByCategory(Category lookedCategory) {
+        List<Recipe> recipesWithLookedCategory = new ArrayList<>();
+        for(Recipe recipe : recipesList) {
+        Category recipeCategory = recipe.getCategory();
+        if(recipeCategory.equals(lookedCategory) {
+        recipesWithLookedCategory.add(recipe);
+        }
+        }
+        return recipesWithLookedCategory;
+        }
+        }*/
+}
